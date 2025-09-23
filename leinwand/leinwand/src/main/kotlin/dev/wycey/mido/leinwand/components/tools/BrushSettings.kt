9e81ef379b823d756d23b0b69c9e52fc05e30c2e
Mido <mido@wycey.dev>
2025-09-23 10:37:21 +0900

    chore(leinwand): Partially remove fraiselait support

--- leinwand/leinwand/src/main/kotlin/dev/wycey/mido/leinwand/components/tools/BrushSettings.kt
+++ leinwand/leinwand/src/main/kotlin/dev/wycey/mido/leinwand/components/tools/BrushSettings.kt
@@ -4,7 +4,6 @@ import dev.wycey.mido.leinwand.Styles
 import dev.wycey.mido.leinwand.tools.brush.Brush
 import dev.wycey.mido.pui.components.basic.StatelessComponent
 import dev.wycey.mido.pui.components.layout.VStack
-import dev.wycey.mido.pui.components.layout.VirtualBox
 import dev.wycey.mido.pui.components.text.Text
 import dev.wycey.mido.pui.elements.base.BuildContext
 
@@ -15,11 +14,7 @@ internal class BrushSettings(
   override fun build(context: BuildContext) =
     VStack(
       listOf(
-        Text("Size", Styles.text),
-        VirtualBox(
-          height = 80f,
-          child = BrushSizeSettings(brush)
-        )
+        Text("Size", Styles.text)
       )
     )
 }
